28f281d9daf0ba9fd8c136a0585a74a8dc39e373
nikolaikoustov <nikolai.koustov@expleogroup.com>
2026-07-07 12:21:01 +0000

    Additional comments

--- 3- Incremental Data Processing/3.2 - Auto Loader.py
+++ 3- Incremental Data Processing/3.2 - Auto Loader.py
@@ -31,11 +31,11 @@ display(files)
 (spark.readStream
         .format("cloudFiles")
         .option("cloudFiles.format", "parquet")
-        .option("cloudFiles.schemaLocation", f"{checkpoints_bookstore}/orders")
+        .option("cloudFiles.schemaLocation", f"{checkpoints_bookstore}/orders_loader_checkpoint")
         .load(f"{dataset_bookstore}/orders-raw")
       .writeStream
         .trigger(availableNow=True) # we use trigger AvailableNow as Trigger type ProcessingTime is not supported for Serverless compute.
-        .option("checkpointLocation", f"{checkpoints_bookstore}/orders")
+        .option("checkpointLocation", f"{checkpoints_bookstore}/orders_loader_checkpoint")
         .table("orders_updates")
 )
 
@@ -66,6 +66,15 @@ display(files)
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC # Re-running Auto Loader
+# MAGIC When using serverless engine, we need to manually re-run the Auto Loader related cell to load data from the newly added files.
+# MAGIC As we re-ran the Auto Loader usign the same checkpoint, it identified that we already processed one file in the source directory so it only worked on files added after the last checkpoint / we finished processing last time.
+# MAGIC Once done, run the cells below to analyze the orders table.
+# MAGIC
+
+# COMMAND ----------
+
 # MAGIC %sql
 # MAGIC SELECT count(*) FROM orders_updates
 
@@ -89,8 +98,11 @@ display(files)
 # COMMAND ----------
 
 # MAGIC %sql
+# MAGIC -- This command deletes table from catalog but it preserves data in the underlying AWS S3 bucket and you need to delete it manually
 # MAGIC DROP TABLE orders_updates
 
 # COMMAND ----------
 
-dbutils.fs.rm(f"{checkpoints_bookstore}/orders", True)
+# This command deletes files in the checkpoint directory we used in this notebook
+# As the checkpoint dirtectory is kept on a Volume backend by AWS S3 bucket, the files are actually deleted from AWS S3 bucket
+dbutils.fs.rm(f"{checkpoints_bookstore}/orders_loader_checkpoint", True)
